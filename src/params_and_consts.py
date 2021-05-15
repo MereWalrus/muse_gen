866bc7d4c2aa2d2ab93314675da67253f34fcfca
eugenezhavoronok@mail.ru <eugenezhavoronok@mail.ru>
2021-05-16 01:25:14 +0300

    change params

--- src/params_and_consts.py
+++ src/params_and_consts.py
@@ -5,15 +5,16 @@ NUM_EVENTS = 388
 vocab = list(range(NUM_EVENTS))
 vocab_size = len(vocab)
 
-num_files = 500
-num_iters = 200
+num_files = 1000
+num_iters = 5000
 seq_length = 100
 
 rnn_units = 1024
 embedding_dim = 256 
 batch_size = 64
-learning_rate = 5e-3
+learning_rate = 5e-4
 
 start_event = 47
+generation_length = 100
 
 save_path = os.path.join("..", "model.h5")
